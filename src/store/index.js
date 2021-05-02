7d78c49091d0f4d8355ab59488569215dc454a67
Andrey Yolkin <andreyyolkin@gmail.com>
2021-05-02 19:46:36 +0300

    First store implementation

--- src/store/index.js
+++ src/store/index.js
@@ -4,9 +4,25 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
+  state: () => ({
+    ball: {
+      flag: 'EU_0',
+      back: 0,
+      customBack: false,
+      eye: 0,
+      accessories: [],
+    }
+  }),
   mutations: {
+    setFlag(state, data) {
+      state.ball.flag = data
+    },
+    setBack(state, data) {
+      state.ball.flag = data
+    },
+    setEye(state, data) {
+      state.ball.flag = data
+    },
   },
   actions: {
   },
